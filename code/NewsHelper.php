ef6f723bddc3b388abb0b79cc78702620a155bcc
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:02:57 +1300

    Converted to PSR-2

--- code/NewsHelper.php
+++ code/NewsHelper.php
@@ -5,27 +5,31 @@
 class NewsHelper
 {
 
-	/**
-	 * Get news items
-	 *
-	 * @param int $offset
-	 * @param int $maxitems Max number of items to return
-	 * @param string $namespace
-	 * @return DataList
-	 */
-	public static function Entries($offset=0, $maxitems=5, $namespace=null) {
-		if($namespace == '*') $namespace = null;
-		$filters = array();
-		if($namespace) {
-			$namespaces = explode(',',$namespace);
-			if($namespaces = array_filter($namespaces)) {
-				if($newsholders = DataObject::get('NewsHolder')->filter(array('Namespace' => $namespaces))) {
-					$filters['ParentID'] = array();
-					foreach($newsholders as $newsholder) $filters['ParentID'][] = $newsholder->ID;
-				}
-			}
-		}
-		return DataObject::get('NewsItem')->filter($filters)->sort('Date DESC')->limit($maxitems, $offset);
-	}
-
+    /**
+     * Get news items
+     *
+     * @param int $offset
+     * @param int $maxitems Max number of items to return
+     * @param string $namespace
+     * @return DataList
+     */
+    public static function Entries($offset=0, $maxitems=5, $namespace=null)
+    {
+        if ($namespace == '*') {
+            $namespace = null;
+        }
+        $filters = array();
+        if ($namespace) {
+            $namespaces = explode(',', $namespace);
+            if ($namespaces = array_filter($namespaces)) {
+                if ($newsholders = DataObject::get('NewsHolder')->filter(array('Namespace' => $namespaces))) {
+                    $filters['ParentID'] = array();
+                    foreach ($newsholders as $newsholder) {
+                        $filters['ParentID'][] = $newsholder->ID;
+                    }
+                }
+            }
+        }
+        return DataObject::get('NewsItem')->filter($filters)->sort('Date DESC')->limit($maxitems, $offset);
+    }
 }
